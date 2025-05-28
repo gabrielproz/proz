27b5cd2e6c60eaaf92421db3d821f025b371c230
gabrielproz <gabrielrodriguesribeiro78@gmail.com>
2025-05-28 09:18:57 -0300

    Delete atividades/atividade_avaliativa directory

--- atividades/atividade_avaliativa/cafe.py
+++
@@ -1,4 +0,0 @@
-café = int(input("qual o valor do cafe"))
-quantidade_cafe = int(input("quantos cafés voce deseja?"))
-total = quantidade_cafe*café
-print("o valor total do seu pedido e: R$ ", total)
