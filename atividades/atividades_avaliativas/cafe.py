c2e7064573a855ba360334f8ed5ff0fb6d93616e
gabrielproz <gabrielrodriguesribeiro78@gmail.com>
2025-05-28 09:19:51 -0300

    Create cafe.py

---
+++ atividades/atividades_avaliativas/cafe.py
@@ -0,0 +1,8 @@
+café = int(input("qual o valor do cafe"))
+quantidade_cafe = int(input("quantos cafés voce deseja?"))
+total = quantidade_cafe*café
+print("o valor total do seu pedido e: R$ ", total)
+ 
+
+
+    
